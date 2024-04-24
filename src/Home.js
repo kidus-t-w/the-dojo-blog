a98181082c1bf7da2fb675f0ea9b10c93ae430fb
kidus-t-w <kidusk69@gmail.com>
2024-04-24 18:19:44 +0300

    Added useEffect to fetch json data

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,31 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
+    const [ blogs, setBlogs] = useState([
+        {title: 'My new website', body: 'lorem lorem...', author: 'mario', id:1},
+        {title: 'Welcome party!', body: 'lorem lorem...', author: 'Yoshi', id:2},
+        {title: 'Web dev top tips', body: 'lorem lorem...', author: 'mario', id:3},
+    ])
 
-    const handleClick = (e) => {
-        console.log('hello, ninjas', e)
-    }
+    const [name, setName] = useState('Kidus')
 
-    const handelClickAgain = (name)=> {
-        console.log('hello', name);
-    }
-    return (
-        <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click me</button>
-            <button onClick={()=>handelClickAgain('mario')}>Click me again</button>
-        </div>
-    );
-}
- 
-export default Home
+    useEffect(() => {
+        fetch('http://localhost:3000/blogs')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setBlogs(data);
+        })
+    }, []);
+  return (
+    <div className="home">
+       {blogs && <BlogList blogs = {blogs} title = "All Blog" />}
+       <button onClick={() => setName('leweji')}>change name</button>
+       <p>{name}</p>
+    </div>
+  );
+};
+
+export default Home;
