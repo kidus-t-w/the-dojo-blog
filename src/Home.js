42bf5a0a81d7baee51a981e16e0dc57f3fce4f67
kidus-t-w <kidusk69@gmail.com>
2024-05-06 14:23:00 +0300

    Added error handler

--- src/Home.js
+++ src/Home.js
@@ -2,28 +2,34 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [ blogs, setBlogs] = useState([
-        {title: 'My new website', body: 'lorem lorem...', author: 'mario', id:1},
-        {title: 'Welcome party!', body: 'lorem lorem...', author: 'Yoshi', id:2},
-        {title: 'Web dev top tips', body: 'lorem lorem...', author: 'mario', id:3},
-    ])
-
-    const [name, setName] = useState('Kidus')
+    const [ blogs, setBlogs] = useState(null)
+    const [isPending, setIsPending] = useState(true)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
-        fetch('http://localhost:3000/blogs')
+        setTimeout(() => fetch('http://localhost:3000/blogs')
         .then(res => {
+            if (!res.ok){
+                throw Error('could not fetch the data');
+            }
             return res.json();
         })
         .then(data => {
             setBlogs(data);
+            setIsPending(false);
+            setError(null);
         })
+        .catch(err => {
+            setIsPending(false);
+            setError(err.message)
+        })
+        , 1000)
     }, []);
   return (
     <div className="home">
+        { error && <div>{error}</div>}
+        {isPending && <div>loading...</div>}
        {blogs && <BlogList blogs = {blogs} title = "All Blog" />}
-       <button onClick={() => setName('leweji')}>change name</button>
-       <p>{name}</p>
     </div>
   );
 };
